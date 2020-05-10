b5de305ae6bc9ad23a33b1f24c3c84a4f9298820
Anton Shpakovsky <shpakovsky86@gmail.com>
2020-05-10 05:41:46 +0300

    Initial Commit

---
+++ one hundred pixels/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  one hundred pixels
+//
+//  Created by Антон Шпаковский on 10.05.2020.
+//  Copyright © 2020 Anton Shpakovsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
